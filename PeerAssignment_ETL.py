7d635039857526650e975624a1adb9789c57f615
annevesth0 <133343720+annevesth0@users.noreply.github.com>
2023-05-12 10:02:07 +0100

    PeerAssignment_ETL.py

---
+++ PeerAssignment_ETL.py
@@ -0,0 +1,9 @@
+!mamba install pandas==1.3.3 -y
+!mamba install requests==2.26.0 -y
+import glob
+import pandas as pd
+from datetime import datetime
+
+!wget https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0221EN-SkillsNetwork/labs/module%206/Lab%20-%20Extract%20Transform%20Load/data/bank_market_cap_1.json
+!wget https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0221EN-SkillsNetwork/labs/module%206/Lab%20-%20Extract%20Transform%20Load/data/bank_market_cap_2.json
+!wget https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0221EN-SkillsNetwork/labs/module%206/Final%20Ass
